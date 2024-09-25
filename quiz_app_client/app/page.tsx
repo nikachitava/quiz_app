a5f43cdbd4bf34b1bfdab8c03024e99d7841cd6e
nikachitava <chitava18.official@gmail.com>
2024-09-25 20:47:42 +0400

    create start game button from start

--- quiz_app_client/app/page.tsx
+++ quiz_app_client/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import NavBar from "@/components/ui/custom/NavBar";
 import QuestionContainer from "@/components/ui/custom/QuestionContainer";
+import { useState } from "react";
 
 export default function Home() {
+	const [startGame, setStartGame] = useState(false);
+
+	const handleStartGame = () => {
+		setStartGame((startGame) => !startGame);
+	};
 	return (
 		<div className="max-h-screen">
 			<NavBar />
-			<QuestionContainer />
+			<div className="container mt-20">
+				<Button onClick={handleStartGame}>
+					{startGame ? "Leave Game" : "Start Game"}
+				</Button>
+			</div>
+			{startGame && <QuestionContainer />}
 		</div>
 	);
 }
--- quiz_app_client/components/ui/custom/NavBar.tsx
+++ quiz_app_client/components/ui/custom/NavBar.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
 	return (
 		<header className="container ">
 			<div className="flex  justify-between items-center py-4">
-				<h1 className="text-primary text-3xl">QUIZ APP</h1>
+				<Link href={"/"}>
+					<h1 className="text-primary text-3xl font-bold">
+						QUIZ APP
+					</h1>
+				</Link>
 				<p>Sing in</p>
 			</div>
 		</header>
